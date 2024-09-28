5b2f15e11df023e8d3b632ef773c0036e89a6d23
vladg <vlad.grush.99@gmail.com>
2024-09-28 19:17:57 +0300

    provide credential for web3

--- FirstAirdropFerma/BlockchainInteraction/BlockchainClient.cs
+++ FirstAirdropFerma/BlockchainInteraction/BlockchainClient.cs
@@ -1,6 +1,16 @@
-﻿namespace FirstAirdropFerma.BlockchainInteraction;
+﻿using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+
+namespace FirstAirdropFerma.BlockchainInteraction;
 
 public class BlockchainClient
 {
-    
+    private readonly Account _account;
+    private readonly Web3 _web3;
+
+    public BlockchainClient(string privateKey, string rpcUrl)
+    {
+        _account = new Account(privateKey);
+        _web3 = new Web3(_account, rpcUrl);
+    }
 }
