385b3c11ba55654685da21c97a848247a37c02e9
vladg <vlad.grush.99@gmail.com>
2024-10-07 19:52:01 +0300

    to protected

--- FirstAirdropFerma/NetworkInteraction/HttpClientFactory/BaseHttpClient.cs
+++ FirstAirdropFerma/NetworkInteraction/HttpClientFactory/BaseHttpClient.cs
@@ -4,7 +4,7 @@ public class BaseHttpClient
 {
     public HttpClient HttpClient { get; set; }
     
-    public void SetDefaultHeaders()
+    protected void SetDefaultHeaders()
     {
         HttpClient.DefaultRequestHeaders.Add("Accept", new []{"application/json", "text/plain", "*/*"});
         HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
